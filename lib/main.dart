80dbaef726f3c17c8aab14668a2b6c6cc471c1fc
Hyuntoe Cho <hyuntoe@gmail.com>
2022-03-27 18:00:03 +0900

    incomplete 23:

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,22 @@ class MyApp extends StatelessWidget {
     // start from here
     return MaterialApp(
         // landing page is home
-        home:
-            // basic widget:
-            // Text('string') : print string
-            // Icon(Icons.{name}) : print icon, icon name search : flutter api document https://api.flutter.dev/flutter/material/Icons-class.html
-            // Image.asset('PATH') : img must be in 'assets' folder and img's path must be registered to pubspec.yaml(flutter: assets: - assets/)
-            // Container() : alternative is SizedBox(), units of width/height is LP ( 1.2cm approx.)
-            Center(child: Container(width: 2, height: 2, color: Colors.blue)));
+        home: Scaffold(
+      // basic widget:
+      // Text('string') : print string
+      // Icon(Icons.{name}) : print icon, icon name search : flutter api document https://api.flutter.dev/flutter/material/Icons-class.html
+      // Image.asset('PATH') : img must be in 'assets' folder and img's path must be registered to pubspec.yaml(flutter: assets: - assets/)
+      // Container() : alternative is SizedBox(), units of width/height is LP ( 1.2cm approx.)
+      appBar: AppBar(),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.star),
+          Text('Star'),
+          Image.asset('assets/DSC03730.JPG')
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(),
+    ));
   }
 }
