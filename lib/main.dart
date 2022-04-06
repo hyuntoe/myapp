f74bcd9e9ebb8dd4c7973644886bc4db7cbba480
Hyuntoe Cho <hyuntoe@gmail.com>
2022-04-06 16:44:22 +0900

    lecture 3 done

--- lib/main.dart
+++ lib/main.dart
@@ -19,24 +19,27 @@ class MyApp extends StatelessWidget {
         //   bottomNavigationBar: BottomAppBar(),
         // )
         home: Scaffold(
-            // basic widget:
-            // Text('string') : print string
-            // Icon(Icons.{name}) : print icon, icon name search : flutter api document https://api.flutter.dev/flutter/material/Icons-class.html
-            // Image.asset('PATH') : img must be in 'assets' folder and img's path must be registered to pubspec.yaml(flutter: assets: - assets/)
-            // Container() : alternative is SizedBox(), units of width/height is LP ( 1.2cm approx.)
-            appBar: AppBar(
-              title: Text('앱임'),
-            ),
-            body: Text('안녕'),
-            bottomNavigationBar: BottomAppBar(
-                child: SizedBox(
-                    height: 70,
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Icon(Icons.phone),
-                          Icon(Icons.message),
-                          Icon(Icons.contact_page),
-                        ])))));
+      // basic widget:
+      // Text('string') : print string
+      // Icon(Icons.{name}) : print icon, icon name search : flutter api document https://api.flutter.dev/flutter/material/Icons-class.html
+      // Image.asset('PATH') : img must be in 'assets' folder and img's path must be registered to pubspec.yaml(flutter: assets: - assets/)
+      // Container() : alternative is SizedBox(), units of width/height is LP ( 1.2cm approx.)
+      appBar: AppBar(
+        title: Text('앱임'),
+      ),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: Container(
+          width: double.infinity,
+          height: 50,
+          margin: EdgeInsets.all(20),
+          padding: EdgeInsets.all(5),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black),
+          ),
+          child: Text('로렐입숨블 라블라'),
+        ),
+      ),
+    ));
   }
 }
