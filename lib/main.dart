b2e59a7c6a352de11dbdbd967c9385e2cab67a3b
Hyuntoe Cho <hyuntoe@gmail.com>
2022-04-06 16:24:44 +0900

    youtube 2nd lecture homework answer of lecturer

--- lib/main.dart
+++ lib/main.dart
@@ -12,32 +12,31 @@ class MyApp extends StatelessWidget {
     // start from here
     return MaterialApp(
         // landing page is home
+        // Basic Structure is below
+        // home: Scaffold(
+        //   appBar: AppBar( title: Text('AppTitle')),
+        //   body: Text('contents'),
+        //   bottomNavigationBar: BottomAppBar(),
+        // )
         home: Scaffold(
-      // basic widget:
-      // Text('string') : print string
-      // Icon(Icons.{name}) : print icon, icon name search : flutter api document https://api.flutter.dev/flutter/material/Icons-class.html
-      // Image.asset('PATH') : img must be in 'assets' folder and img's path must be registered to pubspec.yaml(flutter: assets: - assets/)
-      // Container() : alternative is SizedBox(), units of width/height is LP ( 1.2cm approx.)
-      appBar: AppBar(
-        title: Text('앱임'),
-      ),
-      body: Row(children: [
-        Text('안녕'),
-      ]),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(
-          label: '',
-          icon: (Icon(Icons.phone)),
-        ),
-        BottomNavigationBarItem(
-          label: '',
-          icon: (Icon(Icons.message)),
-        ),
-        BottomNavigationBarItem(
-          label: '',
-          icon: (Icon(Icons.contact_page)),
-        ),
-      ]),
-    ));
+            // basic widget:
+            // Text('string') : print string
+            // Icon(Icons.{name}) : print icon, icon name search : flutter api document https://api.flutter.dev/flutter/material/Icons-class.html
+            // Image.asset('PATH') : img must be in 'assets' folder and img's path must be registered to pubspec.yaml(flutter: assets: - assets/)
+            // Container() : alternative is SizedBox(), units of width/height is LP ( 1.2cm approx.)
+            appBar: AppBar(
+              title: Text('앱임'),
+            ),
+            body: Text('안녕'),
+            bottomNavigationBar: BottomAppBar(
+                child: SizedBox(
+                    height: 70,
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(Icons.phone),
+                          Icon(Icons.message),
+                          Icon(Icons.contact_page),
+                        ])))));
   }
 }
